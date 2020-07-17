d8457bf51286d767701541f0a8c8b58463ab11c3
LouiseOtt <67265974+LouiseOtt@users.noreply.github.com>
2020-07-17 14:56:52 +0200

    ISCC-2020-MyDevBlog

--- page-accueil.php
+++ page-accueil.php
@@ -14,18 +14,7 @@ Je m'appelle Louise OTT, je suis en première année <i>(bientôt deuxième héh
 J'espère que vous aussi, et si c'est le cas, vous êtes au bon endroit pour en apprendre un maximum !
 <br>Venez consulter les articles pour assouvir votre soif de savoir<br><br>
     <div class="bloc">
-        <center>
     <h6>Les derniers articles</h6>
-      <?php 
-      $bdd = new PDO("mysql:host=localhost;dbname=ISCC-2020-MyDevBlog;charset=utf8", "root", "");
-      $articles = $bdd->query('SELECT * FROM articles ORDER BY date_time_post DESC LIMIT 0,5');
-      $lastBuildDate = $bdd->query('SELECT date_time_post FROM articles ORDER BY date_time_post DESC LIMIT 0,1');
-        $lastBuildDate = $lastBuildDate->fetch()['date_time_post'];
-      
-      while($a = $articles->fetch()) { ?>
-      <a href="article.php?id=<?= $a['id'] ?>"><?= $a['titre'] ?></a><br>
-      <?php } ?>
-      </center>
-      </div>
+    
 </body>
 </html>
